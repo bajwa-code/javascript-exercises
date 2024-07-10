c31dec081db6bb783632719e62c39cbfe742664e
rashpal <47848174+bajwa-code@users.noreply.github.com>
2024-07-10 19:28:59 +0530

    print 'hey' 3 times successfully

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
 const repeatString = function() {
-
+    let heyString = '';
+    for (i = 0; i<3; i++) {
+        heyString += 'hey';
+    }
+    return heyString;
 };
 
 // Do not edit below this line
