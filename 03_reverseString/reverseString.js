835af8a6f1640f9301c0177e4d0fae90bd8cd83e
rashpal <47848174+bajwa-code@users.noreply.github.com>
2024-07-11 11:45:18 +0530

    Reverse string with array 1. Convert string to array with inbuilt function split() 2. Reverse converted array with function reverse() 3. Convert back reversed array to string with join()

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    const strArray = word.split('');
+    const reversedArray = strArray.reverse();
+    const reversedStr = reversedArray.join('');
 
+    return reversedStr;
 };
 
 // Do not edit below this line
